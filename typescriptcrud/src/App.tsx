e5889b05c1cdbf49e12647b2bdb884025431add7
Marcos Sousa <96206825+marcossantossousa@users.noreply.github.com>
2023-01-19 16:26:12 -0300

    Setup

---
+++ typescriptcrud/src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          <div className="title">Typescript CRUD</div>
+          <div className="subtitle">Com React e Typescript</div>
+        </h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
